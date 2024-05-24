7abab5ab7502ead678cfa87faca8affb5abba481
Justin Bachtell <justin@justinbachtell.com>
2024-05-24 07:27:47 -0500

    Add viewport rules to prevent zooming when inputs are focused

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,6 +81,11 @@ export const viewport: Viewport = {
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: false,
 };
 
 export default async function RootLayout({
